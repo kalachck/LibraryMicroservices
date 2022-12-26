8ff45396fe73c541bee50d37657bd42adc2907cc
kalachck <swagzilla09112002@gmail.com>
2022-12-26 13:54:29 +0300

    API_EndpointsImplementation: Added valid gitignore, mute controller methods and login model.

---
+++ IdentityService/src/IdentityService.Api/Controllers/AuthorizationController.cs
@@ -0,0 +1,37 @@
+﻿using IdentityService.Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    [Authorize]
+    public class AuthorizationController : ControllerBase
+    {
+        public AuthorizationController()
+        { }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> RegisterAsync(LoginModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("signin")]
+        public async Task<IActionResult> SignInAsync(LoginModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> SignOutAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
