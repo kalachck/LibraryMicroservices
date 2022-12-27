13c2435d2e80ae137bcac4d68faf7082ec4678a0
kalachck <swagzilla09112002@gmail.com>
2022-12-27 16:06:41 +0300

    DataAccessLayer: Added entity and entity configuration, added repositories and application context

---
+++ IdentityServer/src/IdentityServer.DataAccess/Entities/UserEntity.cs
@@ -0,0 +1,14 @@
+﻿using IdentityServer.DataAccess.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityServer.DataAccess.Entities
+{
+    public class UserEntity : IdentityUser<int>
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
---
+++ IdentityServer/src/IdentityServer.DataAccess/EntityConfigurations/UserEntityConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using IdentityServer.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdentityServer.DataAccess.EntityConfigurations
+{
+    public class UserEntityConfiguration : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(30)
+                .HasColumnType("nvarchar");
+
+            builder.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(30)
+                .HasColumnType("nvarchar");
+
+            builder.Property(x => x.UserName)
+                .HasColumnName("Login")
+                .IsRequired()
+                .HasMaxLength(30)
+                .HasColumnType("nvarchar");
+
+            builder.Property(u => u.PasswordHash)
+                .HasColumnName("Password")
+                .IsRequired()
+                .HasMaxLength(36)
+                .HasColumnType("varchar");
+
+            builder.Property(x => x.Role)
+                .IsRequired()
+                .HasColumnType("bit");
+
+            builder.Ignore(x => x.NormalizedEmail);
+            builder.Ignore(x => x.EmailConfirmed);
+            builder.Ignore(x => x.NormalizedUserName);
+            builder.Ignore(x => x.SecurityStamp);
+            builder.Ignore(x => x.ConcurrencyStamp);
+            builder.Ignore(x => x.PhoneNumber);
+            builder.Ignore(x => x.PhoneNumberConfirmed);
+            builder.Ignore(x => x.TwoFactorEnabled);
+            builder.Ignore(x => x.LockoutEnabled);
+            builder.Ignore(x => x.LockoutEnd);
+            builder.Ignore(x => x.AccessFailedCount);
+        }
+    }
+}
