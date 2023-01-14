ef7cf537c4c146fe04bf1079e88bc0b89080995c
kalachck <swagzilla09112002@gmail.com>
2023-01-14 17:46:11 +0300

    LibraryServiceDockerComposeImplementation: Changed connection string, migration string and added docker-compose file

--- LibraryService/src/LibraryService.DataAccess/Migrations/20230114094833_Initial3.Designer.cs
+++ LibraryService/src/LibraryService.DataAccess/Migrations/20230114094833_Initial3.Designer.cs
@@ -4,6 +4,7 @@ using LibrarySevice.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibrarySevice.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230114094833_Initial3")]
+    partial class Initial3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
