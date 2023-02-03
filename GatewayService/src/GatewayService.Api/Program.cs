8d1de8b821cfa2c6323a9c6f928cca4c8b2ec968
kalachck <swagzilla09112002@gmail.com>
2023-02-03 18:37:28 +0300

    GatewayServiceImplementation: Added gateway service

---
+++ GatewayService/src/GatewayService.Api/Program.cs
@@ -0,0 +1,30 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace GatewayService.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("ocelot.json", false, true)
+                .AddEnvironmentVariables();
+
+            builder.Services.AddOcelot();
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseOcelot();
+
+            app.Run();
+        }
+    }
+}
