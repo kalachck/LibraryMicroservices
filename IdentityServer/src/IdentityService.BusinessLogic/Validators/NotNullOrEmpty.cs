946c8dd92ee8308cc5072668d9b7a126a0fce11c
kalachck <swagzilla09112002@gmail.com>
2023-03-04 14:00:08 +0300

    IdentityServiceCustomValidationImplementation: Added custom validation using validation attribute

---
+++ IdentityServer/src/IdentityService.BusinessLogic/Validators/NotNullOrEmpty.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService.BusinessLogic.Validators
+{
+    public class NotNullOrEmpty : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            var stringValue = value as string;
+
+            return !string.IsNullOrWhiteSpace(stringValue);
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (IsValid(value) == false)
+            {
+                var errorMessage = $"{FormatErrorMessage(validationContext.DisplayName)} Field mustn't be null or empty";
+
+                throw new ValidationException(errorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
