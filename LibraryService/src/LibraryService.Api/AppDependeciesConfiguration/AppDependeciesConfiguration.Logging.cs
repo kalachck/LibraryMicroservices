3465caa50770fa673ff50e35f09f601b6635f878
kalachck <swagzilla09112002@gmail.com>
2023-03-05 13:58:40 +0300

    LibraryServiceLoggingImplementation: Added information level logging to console using serilog

---
+++ LibraryService/src/LibraryService.Api/AppDependeciesConfiguration/AppDependeciesConfiguration.Logging.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using Serilog.Exceptions;
+
+namespace LibraryService.Api.AppDependeciesConfiguration
+{
+    public static partial class AppDependeciesConfiguration
+    {
+        public static WebApplicationBuilder AddLogging(this WebApplicationBuilder builder)
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.Async(options =>
+                {
+                    options.Console();
+                })
+                .MinimumLevel.Information()
+                .Enrich.WithExceptionDetails()
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddSerilog(logger);
+
+            return builder;
+        }
+    }
+}
